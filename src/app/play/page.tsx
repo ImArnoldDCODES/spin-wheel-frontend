074616cd3f21de459ebce33c55922224b72b7083
ImArnoldDCODES <danielsarnold010@gmail.com>
2024-06-16 23:44:14 +0100

    suspense added

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import TransitionLink from "components/TransitionLink";
 import SpinTheWheel from "components/Wheel";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 
 export default function Index() {
   const [recents, setRecents] = useState<number[]>([]);
@@ -16,41 +16,43 @@ export default function Index() {
   };
 
   return (
-    <main className="min-h-screen px-24 bg-[#FFA500]">
-      <div className="w-[fit-content] flex gap-5 mr-auto pt-[2rem]">
-        <TransitionLink
-          href="/login"
-          label="login"
-          className="uppercase bg-[#FFD700] z-[3] px-4 rounded-lg text-[3rem] mt-[auto] font-bold hover:bg-[#FF0000] hover:text-white"
-        />
-      </div>
-      <div className="flex flex-row items-center mt-24 justify-between">
-        <div className="max-w-[18rem] bg-[#FF4500] text-white rounded-lg px-5 py-5">
-          <h1 style={{ fontFamily: "samurai" }}>
-            Step right up to the most thrilling game of chance on the web!
-            Welcome to our Spin the Wheel website, where every spin holds the
-            promise of exciting prizes and endless fun. Simply give the wheel a
-            spin and watch as it whirls with anticipation, landing on your prize
-            with a satisfying click.{" "}
-          </h1>
+    <Suspense>
+      <main className="min-h-screen px-24 bg-[#FFA500]">
+        <div className="w-[fit-content] flex gap-5 mr-auto pt-[2rem]">
+          <TransitionLink
+            href="/login"
+            label="login"
+            className="uppercase bg-[#FFD700] z-[3] px-4 rounded-lg text-[3rem] mt-[auto] font-bold hover:bg-[#FF0000] hover:text-white"
+          />
         </div>
-        <div>
-          <SpinTheWheel resData={handleRecent} />
+        <div className="flex flex-row items-center mt-24 justify-between">
+          <div className="max-w-[18rem] bg-[#FF4500] text-white rounded-lg px-5 py-5">
+            <h1 style={{ fontFamily: "samurai" }}>
+              Step right up to the most thrilling game of chance on the web!
+              Welcome to our Spin the Wheel website, where every spin holds the
+              promise of exciting prizes and endless fun. Simply give the wheel
+              a spin and watch as it whirls with anticipation, landing on your
+              prize with a satisfying click.{" "}
+            </h1>
+          </div>
+          <div>
+            <SpinTheWheel resData={handleRecent} />
+          </div>
+          <div className="w-[fit-content] bg-[#FF4500] text-white rounded-lg px-5 py-5">
+            <h1
+              className="uppercase underline text-[1.2rem]"
+              style={{ fontFamily: "wonder" }}
+            >
+              Recent
+            </h1>
+            <ul className="" style={{ fontFamily: "samurai" }}>
+              {recents.slice(-5)?.map((data: number, index) => (
+                <li key={index}>{data} (18s ago)</li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div className="w-[fit-content] bg-[#FF4500] text-white rounded-lg px-5 py-5">
-          <h1
-            className="uppercase underline text-[1.2rem]"
-            style={{ fontFamily: "wonder" }}
-          >
-            Recent
-          </h1>
-          <ul className="" style={{ fontFamily: "samurai" }}>
-            {recents.slice(-5)?.map((data: number, index) => (
-              <li key={index}>{data} (18s ago)</li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 }
