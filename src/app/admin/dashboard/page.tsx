9c23233c6732b26b9a96e620ea91ae1c68fa2b22
ImArnoldDCODES <danielsarnold010@gmail.com>
2025-01-04 21:24:44 +0100

    Added suspense

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -7,6 +7,7 @@ import { useContext, useState, useEffect, Key } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import NavContent from "@/components/NavContent";
 import Image from "next/image";
+import { Suspense } from "react";
 
 interface Giveaway {
   _id: string;
@@ -63,60 +64,62 @@ export default function Index() {
   };
 
   return (
-    <main className="bg-bgcream h-screen relative">
-      <section className="absolute w-full h-full">
-        <NavContent />
-      </section>
-      <section className="w-full h-full flex flex-col lg:flex-row">
-        <div className="lg:w-1/2 w-full sm:px-12 px-5 min-h-full">
-          <div className="bg-desktopcream mt-24 sm:h-[40%] h-[30%] rounded-2xl relative">
-            <Image
-              src="/pie-3.png"
-              alt="thridpie"
-              width={100}
-              height={100}
-              className="h-full sm:w-[15rem] w-[40%] z-[1] absolute right-0 rounded-2xl"
-            />
-            <div className="flex flex-col pl-5 py-5 justify-between items-start align-top h-full w-2/3 z-[2] relative">
-              <h1 className="font-semibold font-cooper text-[2.2rem] text-moondark">
-                Welcome, {profile?.name.split(" ")}
-              </h1>
-              <button className="bg-cream rounded-3xl sm:h-12 sm:w-[12rem] h-10 w-[10rem] text-lg sm:text-2xl font-semibold font-cooper text-moondark">
-                Create Wheel
-              </button>
+    <Suspense>
+      <main className="bg-bgcream h-screen relative">
+        <section className="absolute w-full h-full">
+          <NavContent />
+        </section>
+        <section className="w-full h-full flex flex-col lg:flex-row">
+          <div className="lg:w-1/2 w-full sm:px-12 px-5 min-h-full">
+            <div className="bg-desktopcream mt-24 sm:h-[40%] h-[30%] rounded-2xl relative">
+              <Image
+                src="/pie-3.png"
+                alt="thridpie"
+                width={100}
+                height={100}
+                className="h-full sm:w-[15rem] w-[40%] z-[1] absolute right-0 rounded-2xl"
+              />
+              <div className="flex flex-col pl-5 py-5 justify-between items-start align-top h-full w-2/3 z-[2] relative">
+                <h1 className="font-semibold font-cooper text-[2.2rem] text-moondark">
+                  Welcome, {profile?.name.split(" ")}
+                </h1>
+                <button className="bg-cream rounded-3xl sm:h-12 sm:w-[12rem] h-10 w-[10rem] text-lg sm:text-2xl font-semibold font-cooper text-moondark">
+                  Create Wheel
+                </button>
+              </div>
+            </div>
+            <div className="sm:h-[40%] h-[55%] mt-4 overflow-y-auto relative custom-scrollbar bg-transparent">
+              <ul>
+                {profile?.giveaways.map((data: any) => (
+                  <li
+                    key={data.winners._id}
+                    className="bg-desktopcream flex text-center items-center justify-between px-6 sm:h-10 h-14 rounded-md font-cooper cursor-pointer mb-3 w-full"
+                  >
+                    <h2 className="text-moondark text-start hover:cursor-pointer w-[40%]">
+                      {data.title}
+                    </h2>
+                    <h6 className="text-moondark text-opacity-50 w-[20%]">
+                      {handleDate(data.date)}
+                    </h6>
+                    <h4 className="text-moondark text-opacity-50 w-[20%]">
+                      {data.winners?.[0]?.name}
+                    </h4>
+                    <Image
+                      src="/expand.png"
+                      alt="expand"
+                      width={100}
+                      height={100}
+                      className="w-4 h-4 cursor-pointer"
+                    />
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div className="sm:h-[40%] h-[55%] mt-4 overflow-y-auto relative custom-scrollbar bg-transparent">
-            <ul>
-              {profile?.giveaways.map((data: any) => (
-                <li
-                  key={data.winners._id}
-                  className="bg-desktopcream flex text-center items-center justify-between px-6 sm:h-10 h-14 rounded-md font-cooper cursor-pointer mb-3 w-full"
-                >
-                  <h2 className="text-moondark text-start hover:cursor-pointer w-[40%]">
-                    {data.title}
-                  </h2>
-                  <h6 className="text-moondark text-opacity-50 w-[20%]">
-                    {handleDate(data.date)}
-                  </h6>
-                  <h4 className="text-moondark text-opacity-50 w-[20%]">
-                    {data.winners?.[0]?.name}
-                  </h4>
-                  <Image
-                    src="/expand.png"
-                    alt="expand"
-                    width={100}
-                    height={100}
-                    className="w-4 h-4 cursor-pointer"
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="w-1/2 bg-desktopcream"></div>
-      </section>
-    </main>
+          <div className="w-1/2 bg-desktopcream"></div>
+        </section>
+      </main>
+    </Suspense>
 
     // <main className="flex h-screen w-screen flex-col md:flex-row">
     //   <Navbar />
